dccca9ebc7dee7c43181cc46e5d3e80abf0f6409
filippettijuanm <filippetti_juanm@hotmail.com>
2024-09-01 09:55:47 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Benefits from "./Screens/Benefits";
 import Contact from "./Screens/Contact";
 import Home from "./Screens/Home";
 import HowWeWork from "./Screens/HowWeWork";
 import Services from "./Screens/Services";
-import NavBar from "./Components/NavBar";
+import NavBar from "./Components/NavBar"
 
 function App() {
   return (
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react"
+import { Link } from "react-scroll";
+import styles from "./NavBar.module.css";
+import { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
 
-const Navbar = () => {
+const NavBar = () => {
+  const [navBarOpen, setNavBarOpen] = useState(false)
+  const links = [
+    {
+      id: 1,
+      link: "Home",
+    },
+    {
+      id: 2,
+      link: "Services",
+    },
+    {
+      id: 3,
+      link: "HowWeWork",
+    },
+    {
+      id: 4,
+      link: "Benefits",
+    },
+  ]
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className={!navBarOpen === true ? styles.navBar : styles.navOpen}>
+      {!navBarOpen && <p className={styles.logo}>Yoga Natura</p>}
+      {!navBarOpen ? (
+        <AiOutlineMenu onClick={() => setNavBarOpen(!navBarOpen)} size={25} />
+      ) : (
+        <AiOutlineClose onClick={() => setNavBarOpen(!navBarOpen)} size={25} />
+      )}
+      {navBarOpen && (
+        <ul>
+          {links.map ( (x) => (
+            <div>
+              <Link onClick={() => setNavBarOpen(false)} to={x.link} smooth duration={500} className={styles.navLink}> {x.link === "HowWeWork" ? "How we work" : x.link}</Link>
+            </div>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
-export default Navbar
+export default NavBar;
